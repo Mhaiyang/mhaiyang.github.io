a79654db2b4a3b9e962a22b2e65542d6ffccf26d
TaylorMei <845879017@qq.com>
2021-08-29 17:50:18 +0800

    2021.08.29

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-import emailjs from 'emailjs-com'
-import apiKeys from './apikeys'
+//import emailjs from 'emailjs-com'
+//import apiKeys from './apikeys'
+//
+//const sendEmail = e = >{
+//  e.preventDefault() emailjs.sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID).then(result = >{
+//    console.log(result.text)
+//  },
+//  error = >{
+//    console.log(error.text)
+//  })
+//}
+//
+//const form = document.querySelector('.form')form.addEventListener('submit',sendEmail)
 
-const sendEmail = e = >{
-  e.preventDefault() emailjs.sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID).then(result = >{
-    console.log(result.text)
-  },
-  error = >{
-    console.log(error.text)
-  })
-}
+$('#btnSubmit').click(function(e){
+    var templateParams = {
+        to_name: $('#first').val(),
+        from_name: $('#second').val(),
+        message: $('#third').val()
+    };
+    emailjs.send('service_xbodefd', 'CVPR2021_PDNet', templateParams)
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert("Your Request has been Sent!");
+    }, function(error) {
+       console.log('FAILED...', error);
+       alert("Your Request failed!")
+    });
+});
 
-const form = document.querySelector('.form')form.addEventListener('submit',sendEmail)

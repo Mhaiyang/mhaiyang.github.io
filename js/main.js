376b0a2321917a1a18f42191199de048cc5c68d8
TaylorMei <845879017@qq.com>
2021-08-29 19:11:08 +0800

    2021.08.29

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-//import emailjs from 'emailjs-com'
-//import apiKeys from './apikeys'
-//
-//const sendEmail = e = >{
-//  e.preventDefault() emailjs.sendForm('gmail', apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID).then(result = >{
-//    console.log(result.text)
-//  },
-//  error = >{
-//    console.log(error.text)
-//  })
-//}
-//
-//const form = document.querySelector('.form')form.addEventListener('submit',sendEmail)
-
 $('#btnSubmit').click(function(e){
     var templateParams = {
         name: $('#first').val(),
@@ -27,3 +13,4 @@ $('#btnSubmit').click(function(e){
        alert("Your Request failed!")
     });
 });
+
